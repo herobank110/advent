ddc01db09d27757df9c2af7a7051954fdf022a0f
David Kanekanian <dkanekanian1@gmail.com>
2024-11-30 16:19:48 +0000

    part 2

--- 2023/01.py
+++ 2023/01.py
@@ -1,1 +1,22 @@
-print(sum(int((lambda b: b[0] + b[-1])(list(filter(str.isdigit, a)))) for a in open('01.txt').readlines()))
+print(
+    sum(
+        int((lambda b: b[0] + b[-1])(list(filter(str.isdigit, a))))
+        for a in open("01.txt").readlines()
+    )
+)
+
+
+import re
+
+z = "one two three four five six seven eight nine".split()
+print(
+    sum(
+        (lambda c: c[0] * 10 + c[-1])(
+            [
+                int(b) if b.isdigit() else z.index(b) + 1
+                for b in re.findall(rf"(?=(\d|{'|'.join(z)}))", a)
+            ]
+        )
+        for a in open("01.txt").readlines()
+    )
+)
