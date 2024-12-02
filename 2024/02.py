b65b18897991a441e7c7e8f7ea97b358f837e7f8
David Kanekanian <dkanekanian1@gmail.com>
2024-12-02 22:34:36 +0000

    02

--- 2024/02.py
+++ 2024/02.py
@@ -7,3 +7,20 @@ print(
         for b in open("02.txt").readlines()
     )
 )
+
+
+print(
+    sum(
+        (
+            lambda d=list(map(int, b.split())): any(
+                (
+                    lambda j=d[:], f=__import__("itertools").pairwise: (j.pop(i) and 0)
+                    or len(set(e < f for e, f in f(j))) == 1
+                    and all(1 <= abs(e - f) <= 3 for e, f in f(j))
+                )()
+                for i in range(len(d))
+            )
+        )()
+        for b in open("02.txt").readlines()
+    )
+)
